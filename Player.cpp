d6001082fd3625b3e564edb7bd01793418f96b84
唯人 佐藤 <220079@jc-21.jp>
2024-03-05 17:10:18 +0900

    カメラできかけ。あとは気合！

--- Player.cpp
+++ Player.cpp
@@ -31,7 +31,15 @@ void Player::Initialize(){
 
 //çXêV
 void Player::Update() {
+	float CamdisPlayer = 10.0f;
+	XMFLOAT3 CmPos =
+	{
+		transform_.position_.x,
+		transform_.position_.y,
+		transform_.position_.z - CamdisPlayer
+	};
 
+	Camera::SetPosition(CmPos);
 	Camera::SetTarget(transform_.position_);
 
 	switch (playerstate_)
