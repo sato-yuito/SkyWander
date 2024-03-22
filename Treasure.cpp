0554b2f4c5827fefa1a12c6a2b847145bcbef85b
sato-yuito <220079@jc-21.jp>
2024-03-23 08:36:57 +0900

    きょうでいったんゲームクリアまで作る

--- Treasure.cpp
+++ Treasure.cpp
@@ -1,5 +1,5 @@
 #include "Treasure.h"
-
+#include"Engine/Model.h"
 Treasure::Treasure(GameObject* parent)
 	:GameObject(parent, "treasure"),treasureModel_(-1)
 {
@@ -11,14 +11,21 @@ Treasure::~Treasure()
 
 void Treasure::Initialize()
 {
+	treasureModel_ = Model::Load("Assets/Treasure.fbx");
+	SetPosition(200, 0, 25);
 }
 
-void Treasure::Update()
-{
+void Treasure::Update(){
+	if (FindObject("Player"))
+	{
+
+	}
 }
 
 void Treasure::Draw()
 {
+	Model::SetTransform(treasureModel_, transform_);
+	Model::Draw(treasureModel_);
 }
 
 void Treasure::Release()
