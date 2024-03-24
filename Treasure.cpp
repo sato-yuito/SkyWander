f1bb38b4a700d92decc8c78e3cefdfb324bfee47
sato-yuito <220079@jc-21.jp>
2024-03-24 22:32:08 +0900

    場所変えただけ

--- Treasure.cpp
+++ Treasure.cpp
@@ -16,7 +16,7 @@ void Treasure::Initialize()
 	assert(treasureModel_ >= 0);
 	transform_.scale_ ={ 0.5,0.5,0.5};
 	transform_.rotate_ = { 0,-180,0 };
-	transform_.position_ = { 0,-1,260 };
+	transform_.position_ = { 0,-1,170 };
 
 	SphereCollider* collision = new SphereCollider(XMFLOAT3(0, 0, 0), 2.5f);
 	AddCollider(collision);
