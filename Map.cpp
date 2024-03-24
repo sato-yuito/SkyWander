1932614ceee2315e396a19d783b95dabffa517ca
sato-yuito <220079@jc-21.jp>
2024-03-25 00:06:15 +0900

    いったんはできたー

--- Map.cpp
+++ Map.cpp
@@ -13,7 +13,7 @@ void Map::Initialize(){
 	for (int i = 0; i < 10; i++)
 	{
 		Floor* fl = Instantiate<Floor>(this);
-		fl->SetPosition(0, 0, i * 18);
+		fl->SetPosition(0, 0, i * 20);
 		floorData.push_back(fl);
 	}
 	
